30514553cde076fc0c5ff9f04f63ddc1600884a0
foreverchx <hrewego8520@yahoo.com.tw>
2019-06-29 17:33:50 +0800

    java and kotlin var,data type

--- src/main/java/tom/com/hello.java
+++ src/main/java/tom/com/hello.java
@@ -5,6 +5,13 @@ public class hello {
        // System.out.println("Hello world");
       Preson p =new Preson();
       p.hello();
+      int age = 19;
+      char c = '我';
+      byte b = 120;
+      float weight = 66.5f;
+      float height = 1.7f;
+      boolean adult = true;
+      boolean enroll = false;
     }
 
 
--- src/main/kotlin/tom/com/Hello.kt
+++ src/main/kotlin/tom/com/Hello.kt
@@ -2,9 +2,15 @@ package tom.com
 
 fun main(args: Array<String>) {
     //println("Hello kotlin")
- //  People().hello()
-    val p = People()
-    p.hello()
+    //  People().hello()
+  /* val h = People()
+   h.hello()
+    var age : Int =25;
+
+    val weight =66.5
+    val enroll : Boolean = true
+    println(age)*/
+
 }
 
 class People{
