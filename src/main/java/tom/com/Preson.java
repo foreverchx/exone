219312c2f29a286d0f986d0612ff5393d139fe1f
foreverchx <hrewego8520@yahoo.com.tw>
2019-06-29 16:32:08 +0800

    Initial commit

---
+++ src/main/java/tom/com/Preson.java
@@ -0,0 +1,8 @@
+package tom.com;
+
+public class Preson {
+    public void hello()
+    {
+        System.out.println("Hello java");
+    }
+}
---
+++ src/main/kotlin/tom/com/Hello.kt
@@ -0,0 +1,17 @@
+package tom.com
+
+fun main(args: Array<String>) {
+    //println("Hello kotlin")
+ //  People().hello()
+    val p = People()
+    p.hello()
+}
+
+class People{
+  fun hello(){
+      println("Hello kotlin")
+
+  }
+
+
+}
